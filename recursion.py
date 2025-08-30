a145510778ffc3e806c9f62f7253b30e1e4d6745
Ahammed Faisal K <ahammedfaisal70@gmail.com>
2025-08-30 17:10:11 +0530

    Added new problem fibanocci, check palindrom in a string

--- recursion.py
+++ recursion.py
@@ -24,32 +24,59 @@
 
 # print_n_to_1(10)
 
-def print_sum_n_numbers(n, sum=0):
-    if n < 1:
-        return sum
-    sum += n
-    return print_sum_n_numbers(n - 1, sum)
+# def print_sum_n_numbers(n, sum=0):
+#     if n < 1:
+#         return sum
+#     sum += n
+#     return print_sum_n_numbers(n - 1, sum)
 
-print(print_sum_n_numbers(5))
-
-
-def factorial_number(n):
-    if n == 0:
-        return 1
-    return n * factorial_number(n - 1)
+# print(print_sum_n_numbers(5))
 
 
-print(factorial_number(5))
+# def factorial_number(n):
+#     if n == 0:
+#         return 1
+#     return n * factorial_number(n - 1)
 
 
-def reverse_an_array(arr, start, end):
+# print(factorial_number(5))
+
+
+# def reverse_an_array(arr, start, end):
+#     if start >= end:
+#         return 
+#     arr[start], arr[end] = arr[end] , arr[start]
+#     reverse_an_array(arr, start+1, end - 1)
+
+# arr = [2, 4, 3, 21, 55, 33, 20]
+# start = 0
+# end = len(arr) - 1
+# reverse_an_array(arr, start, end)
+# print(arr)
+
+
+def check_palindrome(num, start, end):
     if start >= end:
-        return 
-    arr[start], arr[end] = arr[end] , arr[start]
-    reverse_an_array(arr, start+1, end - 1)
+        return True
+    if num[start] != num[end]:
+        return  False
+    return check_palindrome(num, start + 1, end - 1)
 
-arr = [2, 4, 3, 21, 55, 33, 20]
+text = "goog"
 start = 0
-end = len(arr) - 1
-reverse_an_array(arr, start, end)
-print(arr)
+end = len(text) - 1
+if check_palindrome(text, start, end):
+    print("Its a  palindrome")
+else:
+    print("Not a palindrome")
+
+
+def fibanocci(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    
+    return fibanocci(n - 1) + fibanocci(n - 2)
+
+print(fibanocci(4))
